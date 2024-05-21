21eca36f2f670174759a720b375e132eccdf6423
baytursevval <baytursevval@gmail.com>
2024-05-21 16:57:57 +0300

    delete işlemleri

--- src/main/java/com/sevval/CrudApp/controller/TiyatroController.java
+++ src/main/java/com/sevval/CrudApp/controller/TiyatroController.java
@@ -2,11 +2,13 @@ package com.sevval.CrudApp.controller;
 
 import com.sevval.CrudApp.entity.Tiyatro;
 import com.sevval.CrudApp.service.TiyatroService;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
@@ -18,5 +20,21 @@ public class TiyatroController {
     @PostMapping("/tiyatro")
     public Tiyatro postTiyatro(@RequestBody Tiyatro tiyatro){
         return tiyatroService.postTiyatro(tiyatro);
+    }
+
+    @GetMapping("/tiyatrolar")
+    public List<Tiyatro> getAllTiyatro(){
+        return  tiyatroService.getAllTiyatros();
+    }
+
+    @DeleteMapping("/tiyatro/{id}")
+    public ResponseEntity<?> deleteTiyatroById(@PathVariable("id") Long id)
+    {
+        try{
+        tiyatroService.deleteTiyatro(id);
+        return new ResponseEntity<>(id + "nolu oyun silindi", HttpStatus.OK);
+    } catch (EntityNotFoundException e) {
+        return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
+    }
     }
 }
--- src/main/java/com/sevval/CrudApp/service/TiyatroService.java
+++ src/main/java/com/sevval/CrudApp/service/TiyatroService.java
@@ -2,8 +2,11 @@ package com.sevval.CrudApp.service;
 
 import com.sevval.CrudApp.entity.Tiyatro;
 import com.sevval.CrudApp.repository.TiyatroRepository;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -14,4 +17,18 @@ public class TiyatroService {
     public Tiyatro postTiyatro(Tiyatro tiyatro) {
         return  tiyatroRepository.save(tiyatro);
     }
+
+    public List<Tiyatro> getAllTiyatros(){
+        return tiyatroRepository.findAll();
+    }
+
+    public void deleteTiyatro(Long id){
+        if(!tiyatroRepository.existsById(id)){
+            throw new EntityNotFoundException(id + " ID nolu oyun bulunamadı");
+        }
+        tiyatroRepository.deleteById(id);
+    }
+
+
+
 }
